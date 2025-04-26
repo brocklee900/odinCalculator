06b945e1511e4368fce64a0d6f974ea6603e7f58
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-04-25 20:44:07 -0700

    Add basic functionality to buttons and display
    The buttons must be put in the correct order to work currently,
    i.e. number->operator->number->equals and then display result.

--- javascript.js
+++ javascript.js
@@ -29,6 +29,60 @@ function operate(num1, num2, operator) {
     }
 }
 
-let number1;
-let number2;
+let display = document.querySelector(".display");
+let numbersDiv = document.querySelector(".numbers");
+let operandsDiv = document.querySelector(".operands");
+let number1 = 0;
+let number2 = 0;
 let operator;
+let displayRefresh = false;
+
+numbersDiv.addEventListener("click", (event) => {
+	let id= event.target.getAttribute("id");
+	if (id != null) {
+  	if (display.textContent == "0" || displayRefresh) {
+    	displayRefresh = false;
+    	display.textContent = id;
+		} else {
+			display.textContent += id;
+    }
+  }
+});
+
+operandsDiv.addEventListener("click", (event) => {
+	let id = event.target.getAttribute("id");
+  switch (id) {
+  	case("add"):
+			operator = '+';
+      number1 = +display.textContent;
+      break;
+    case("subtract"):
+			operator = '-';
+      number1 = +display.textContent;
+      break;
+    case("multiply"):
+    	operator = '*';
+      number1 = +display.textContent;
+			break;
+    case("divide"):
+    	operator = '/';
+      number1 = +display.textContent;
+			break;
+    case("calculate"):
+    	number2 = +display.textContent;
+      display.textContent = operate(number1, number2, operator);
+      number1 = 0;
+      number2 = 0;
+      operator = '';
+			break;
+    case("clear"):
+    	number1 = 0;
+      number2 = 0;
+      operator = '';
+      display.textContent = "0";
+			break;
+    default:
+    	console.log("Do nothing");
+	}
+  displayRefresh = true;
+})
