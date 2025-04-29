991bcae9388801ec04a0b65c1006581abf11c670
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-04-28 22:15:05 -0700

    Fix edge cases/bugs in calculator functionality
    - Set a minimum width to the display to avoid large numbers overflowing
    - limit number input to 16 digits (calc accuracy lowers beyond 16)
    - "=" button only works when an operator has been pressed
    - dividing by zero displays a message to the user
    - pressing operators in succession will no longer calculate;
    numbers must be pressed in between operator presses
    - moved some code into functions to look cleaner

--- javascript.js
+++ javascript.js
@@ -1,99 +1,111 @@
 function add(num1, num2) {
-  return num1+num2;
+  return num1 + num2
 }
 
 function subtract(num1, num2) {
-  return num1-num2;
+  return num1 - num2
 }
 
 function multiply(num1, num2) {
-  return num1*num2;
+  return num1 * num2
 }
 
 function divide(num1, num2) {
-  return num1/num2;
+	if (num2 == 0) {
+  	return DIVIDEBYZERO
+	}
+  return num1 / num2
 }
 
 function operate(num1, num2, operator) {
   switch (operator) {
-      case('+'):
-          return add(num1, num2);
-      case('-'):
-          return subtract(num1, num2);
-      case('*'):
-          return multiply(num1, num2);
-      case('/'):
-          return divide(num1, num2);
-      default:
-          console.log("Invalid operator");
+    case "+":
+      return add(num1, num2)
+    case "-":
+      return subtract(num1, num2)
+    case "*":
+      return multiply(num1, num2)
+    case "/":
+      return divide(num1, num2)
+    default:
+      console.log("Invalid operator")
   }
 }
 
-let display = document.querySelector(".display");
-let numbersDiv = document.querySelector(".numbers");
-let operandsDiv = document.querySelector(".operands");
-let number1 = 0;
-let number2 = 0;
-let operator = '';
-let displayRefresh = false;
+
+let display = document.querySelector(".display")
+let numbersDiv = document.querySelector(".numbers")
+let operandsDiv = document.querySelector(".operands")
+let number1 = null
+let number2 = null
+let operator = ""
+let displayRefresh = false
+let operatorLastPressed = false;
+const DIVIDEBYZERO = "bro???"
 
 numbersDiv.addEventListener("click", (event) => {
-let id= event.target.getAttribute("id");
-if (id != null) {
-  if (display.textContent == "0" || displayRefresh) {
-    displayRefresh = false;
-    display.textContent = id;
-  } else {
-    display.textContent += id;
+  let id = event.target.getAttribute("id")
+  if (id != null) {
+    if (display.textContent == "0" || displayRefresh) {
+      displayRefresh = false
+      display.textContent = id
+    } else if (display.textContent.length < 16) {
+      display.textContent += id
+    }
+    operatorLastPressed = false;
   }
-}
-});
+})
 
 operandsDiv.addEventListener("click", (event) => {
-let id = event.target.getAttribute("id");
+  let id = event.target.getAttribute("id")
 
-function checkDoubleOperator () {
-  if (operator != '') {
-    display.textContent = operate(number1, +display.textContent, operator);
+  function checkDoubleOperator() {
+    if (operator != "" && operatorLastPressed == false) {
+      display.textContent = operate(number1, +display.textContent, operator)
+    }
   }
-}
+  
+  function calculate(newOperator) {
+		if (display.textContent != DIVIDEBYZERO) {
+      	checkDoubleOperator()
+        operator = newOperator;
+        number1 = +display.textContent
+        operatorLastPressed = true;
+			}
+  }
 
-switch (id) {
-  case("add"):
-    checkDoubleOperator();
-    operator = '+';
-    number1 = +display.textContent;
-    break;
-  case("subtract"):
-    checkDoubleOperator();
-    operator = '-';
-    number1 = +display.textContent;
-    break;
-  case("multiply"):
-    checkDoubleOperator();
-    operator = '*';
-    number1 = +display.textContent;
-    break;
-  case("divide"):
-    checkDoubleOperator();
-    operator = '/';
-    number1 = +display.textContent;
-    break;
-  case("calculate"):
-    number2 = +display.textContent;
-    display.textContent = operate(number1, number2, operator);
-    number1 = 0;
-    number2 = 0;
-    operator = '';
-    break;
-  case("clear"):
-    number1 = 0;
-    number2 = 0;
-    operator = '';
-    display.textContent = "0";
-    break;
-  default:
-    console.log("Do nothing");
-}
-displayRefresh = true;
+  switch (id) {
+    case "add":
+    	calculate("+")
+      break
+    case "subtract":
+    	calculate("-")
+      break
+    case "multiply":
+    	calculate("*")
+      break
+    case "divide":
+    	calculate("/")
+      break
+    case "calculate":
+			if (number1 != null && operator != '') {
+      	number2 = +display.textContent
+        display.textContent = operate(number1, number2, operator)
+        number1 = 0
+        number2 = 0
+        operator = ""
+        operatorLastPressed = false;
+      }
+      break
+    case "clear":
+      number1 = null
+      number2 = null
+      operator = ""
+      operatorLastPressed = false;
+      display.textContent = "0"
+      break
+    default:
+      console.log("Do nothing")
+  }
+  displayRefresh = true
 })
